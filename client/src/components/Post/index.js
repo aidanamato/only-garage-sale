3283af50716a5f236b4d09385643808bc1411bc4
Ven Kim <venvex@gmail.com>
2021-11-22 20:52:40 -0500

    fixed post button positioning

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -4,15 +4,15 @@ import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 
 const fabStyle = {
-  position: 'absolute',
+  position: 'fixed',
   bottom: 30,
   right: 20,
+  zIndex: 10,
 }
 
 export default function Post() {
   return (
-    <Box 
-      sx={ fabStyle }>
+    <Box sx={ fabStyle }>
       <Fab color="primary" aria-label="add">
         <AddIcon />
       </Fab>
